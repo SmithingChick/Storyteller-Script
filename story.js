85f831bca6bc47217849d7d1adc9d13657b091a2
SmithingChick <sharon.smith.metals@gmail.com>
2015-08-27 19:51:20 -0700

    Initial Upload

---
+++ story.js
@@ -0,0 +1,39 @@
+
+var visitorName = prompt("Hello! What's your name?");
+
+alert("Hi, " + visitorName + ". Let's write a story together.");
+
+var noun1 = prompt("Please choose a noun.");
+
+var womanName = prompt("Please choose a woman's name");
+
+var noun2 = prompt("Please choose another noun.");
+
+var expression = prompt("Please choose an expression.");
+
+place1 = prompt("Please choose a location.");
+
+var manName = prompt("Please choose a man's name.");
+
+var noun3 = prompt("Please choose yet another noun.");
+
+var adj1 = prompt("Please choose an adjective."); /* adjective 1*/
+
+var plNoun1 = prompt("Please choose a plural noun."); /* plural noun*/
+
+var adj2 = prompt("Please choose another adjective."); /* adjective 2*/
+
+var noun4 = prompt("Please choose still another noun.");
+
+var noun5 = prompt("Please choose still yet another noun.");
+
+var noun6 = prompt("Please choose even yet still yet another noun.");
+
+var color = prompt("Please choose a color.");
+
+var verb = prompt("Please choose a verb.");
+
+alert("Thank you, " + visitorName + ". Now let's read our story.");
+
+
+document.write ('It was a dark & stormy ' + noun1 +'. ' + womanName + ' pulled back the ' + noun2 + ' to look out the window. “' + expression + '!” she thought. “Now we’ll never be able to get out of ' + place1 + ' until ' + manName + ' gets here with the ' + noun3 + '.” Outside, a pair of ' + adj1 + ' ' + plNoun1 + ' appeared in the ' + noun4 + '. ' + womanName + ' heard the ' + adj2 + ' call of a ' + noun5 + '. Suddenly, the ' + noun6 + ' burst open and everything went ' + color + '. ' + womanName + ' ' + verb + ' and fainted.');
